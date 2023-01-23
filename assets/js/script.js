70c12dc198f413c550381214699ac82305c1ce35
nshaw973 <nshaw973@gmail.com>
2023-01-22 23:20:04 -0800

    Added a scoreboard to the end of the Code Quiz, so that once all questions have been answered correctly, they can begin to input their score. Currently the log for the scoreboard has yet to be implemented.Another note to add was getting the scoreboard to go away during both a win and a timeout, since the scoreboard is made from appending via Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,24 @@
 //Global Variables-----------------------------//
-var questionNumber = 1;
-console.log(questionNumber)
+var questionNumber = 0;
+console.log(questionNumber);
 //---------------------------------------------//
 //QuerySelectors-------------------------------//
 //Question QuerySelectors----------------------//
-
-var qNum = document.querySelector(".qNum")
-var question = document.querySelector(".question")
-var optionOne = document.querySelector("#o1")
-var optionTwo = document.querySelector("#o2")
-var optionThree = document.querySelector("#o3")
-var optionFour = document.querySelector("#o4")
+ 
+/* var qHeader =document.querySelector(".question-header") */
+var qNum = document.querySelector(".qNum");
+var question = document.querySelector(".question");
+var optionOne = document.querySelector("#o1");
+var optionTwo = document.querySelector("#o2");
+var optionThree = document.querySelector("#o3");
+var optionFour = document.querySelector("#o4");
 
 //-------------------------------------------------------------------------------------------------------//
 //Score, Start Button, and Countdown---------------------------------------------------------------------//
 //---------------------------------------------//
 //Score----------------------------------------//
 
-var pointsEl = document.querySelector("#points")
+var pointsEl = document.querySelector("#points");
 var points = 0;
 
     //resets points to 0 on page refresh//
@@ -34,18 +35,32 @@ function scorePoints() {
 //---------------------------------------------//
 //Start Quiz Button----------------------------//
 
-var startButton = document.querySelector(".startbutton")
+var startButton = document.querySelector(".startbutton");
 
-startButton.addEventListener("click", startQuiz)
+startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
     startButton.textContent = "Start"
+    removeScoreboard();
     clearScTim();
     //keeps button from being clicked again by user mid quiz//
     startButton.disabled = true;
     startTimer();
     showQuestion();
+    addOptionsStyle();
 
+}
+
+//Play again Button----------------------------//
+//appears after all questions answered correctly, or if the timer reaches 0//
+
+function playAgain() {
+    questionNumber = 0;
+    addOptionsStyle();
+    questionReset();
+    clearInterval(timer);
+    startButton.disabled = false;
+    startButton.textContent = "Play Again?";
 }
 
 //clear score and time on play again so that the score and timer don't stack on top of each other//
@@ -73,11 +88,87 @@ function startTimer () {
         //Stops Timer once it reaches 0--------//
 
         if (secondsLeft === 0) {
+            removeScoreboard();
             clearInterval(timer);
+            playAgain();
         }
-        //1000 = to 1 second ------------------//s
+        //1000 = to 1 second ------------------//
     }, 1000);
 }
+
+//Page Styling---------------------------------//
+
+var optionsStyle = document.querySelector(".option-style");
+
+
+function addOptionsStyle (){
+    if(questionNumber === 1) {
+        enterNameBoard.classList.remove("playerName")
+        optionsStyle.classList.remove("opacity-hidden")
+        optionsStyle.classList.add("options");
+    } else if (questionNumber === 6 || secondsLeft === 0){
+        qNum.textContent = ""
+        optionsStyle.classList.remove("options");
+        optionsStyle.classList.add("opacity-hidden");
+    }}
+
+//-------------------------------------------------------------------------------------------------------//
+//Scoreboard log-----------------------------------------------------------------------------------------//
+
+//Enter Your Name ------------------------------------------------------------------//
+//append elements to Div//
+
+var enterNameBoard = document.querySelector(".playerScore")
+var inputField = document.createElement("input");
+var h1El = document.createElement("h1");
+var ScoreEl = document.createElement("h2");
+var timeEl = document.createElement("h2");
+
+
+function enterName () {
+
+    var playerName = document.querySelector(".playerScore");
+    enterNameBoard.classList.add("playerName")
+    playerName.appendChild(h1El);
+    playerName.appendChild(inputField);
+    playerName.appendChild(ScoreEl);
+    playerName.appendChild(timeEl);
+
+    h1El.textContent = "Enter Your Name for the Scoreboard!!!"
+    inputField.setAttribute("type", "email");
+    ScoreEl.classList.add("score")
+    timeEl.classList.add("timeLeft")
+
+    var userScore = document.querySelector(".score");
+    var timeLeftScoreboard = document.querySelector(".timeLeft");
+
+    //added the -5 for secondsLeft, because the final score on the scoreboard kept adding 5 for some odd reason//
+    timeLeftScoreboard.textContent = "Time: " + (secondsLeft - 5);
+    userScore.textContent = "Points: " + points;
+
+    playAgain();
+    enterNameBoard.classList.remove("playername");
+}
+
+//This will remove the scoreboard by removing the elements that were created previously for the html//
+
+function removeScoreboard () {
+    inputField.remove();
+    h1El.remove();
+    ScoreEl.remove();
+    timeEl.remove();
+   };
+
+
+
+//Get final score and have it show up on end screen
+
+
+
+
+
+
+
 
 
 //-------------------------------------------------------------------------------------------------------//
@@ -88,6 +179,7 @@ function startTimer () {
 //This allows for each question to become numbered and to move onto the next question after checking which questions have been answered 
 
 function showQuestion (){
+    questionNumber++;
     console.log(questionNumber)
     if(questionNumber === 1){
         questionOne();
@@ -104,12 +196,20 @@ function showQuestion (){
     if (questionNumber === 5) {
         questionFive();
     }else(!questionFive)
+    if (questionNumber === 6) {
+        clearInterval(timer);
+        //once all questions are done, it runs to the final screen that allows replay//
+        addOptionsStyle();
+        playAgain();
+        enterName();
+
+    }
 };
 
 //Question #1 ------------------------------------------------------------------//
 
 function questionOne() {
-    qNum.textContent = `1`
+    qNum.textContent = `Question: 1`
     question.textContent = "Pick the biggest NUMBER in Javascript"
     optionOne.textContent = '"17"'
     optionTwo.textContent = 'parseInt("14")'
@@ -131,7 +231,7 @@ function questionOne() {
 //Question #2 ------------------------------------------------------------------//
 
 function questionTwo() {
-    qNum.textContent = '2';
+    qNum.textContent = 'Question: 2';
     question.textContent = "How long is 1000 in a setInterval function";
     optionOne.textContent = "10 Seconds";
     optionTwo.textContent = "1000 microseconds";
@@ -152,7 +252,7 @@ function questionTwo() {
 //Question #3 ------------------------------------------------------------------//
 
 function questionThree() {
-    qNum.textContent = '3';
+    qNum.textContent = 'Question: 3';
     question.textContent = "What does the i in a for loop stand for?";
     optionOne.textContent = "iterator";
     optionTwo.textContent = "integer";
@@ -173,7 +273,7 @@ function questionThree() {
 //Question #4 ------------------------------------------------------------------//
 
 function questionFour() {
-    qNum.textContent = '4';
+    qNum.textContent = 'Question: 4';
     question.textContent = "Which logical comparison evaluates for OR";
     optionOne.textContent = "or";
     optionTwo.textContent = "&&";
@@ -193,7 +293,7 @@ function questionFour() {
 //Question #5 ------------------------------------------------------------------//
 
 function questionFive() {
-    qNum.textContent = '5';
+    qNum.textContent = 'Question: 5';
     question.textContent = "Using a console.log(This) selects what?";
     optionOne.textContent = "returns as undefined";
     optionTwo.textContent = "the variable above";
@@ -203,17 +303,7 @@ function questionFive() {
     //Correct Answer --------------------------//
     //Option 4//
 
-    optionThree.addEventListener("click", function(){
-        removeListeners();
-        scorePoints();
-        secondsLeft+=5;
-        questionReset();
-        clearInterval(timer);
-        questionNumber = 1;
-        startButton.disabled = false;
-        startButton.textContent = "Play Again?"
-
-    });
+    optionThree.addEventListener("click", correctAnswer);
 
     optionOne.addEventListener("click", wrongAnswer);
     optionTwo.addEventListener("click", wrongAnswer);
@@ -246,7 +336,6 @@ function correctAnswer () {
     scorePoints();
     secondsLeft+=5;
     questionReset();
-    questionNumber++;
     showQuestion();
 }
 
